49a32464e087ded56d46a80896c20c285aa1b6d6
Coffeecaat <juniya070894@gmail.com>
2025-10-08 17:47:21 +0900

    chore: 스프링 부트 프로젝트 초기화

---
+++ src/main/java/WhatTheBus/WhatTheBusApplication.java
@@ -0,0 +1,13 @@
+package WhatTheBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatTheBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WhatTheBus/WhatTheBusApplicationTests.java
@@ -0,0 +1,14 @@
+package WhatTheBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class WhatTheBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
