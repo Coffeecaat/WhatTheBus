f6d6535568d3e50cd18052bef0e8cc93df2bd4c7
Coffeecaat <juniya070894@gmail.com>
2025-11-21 02:10:31 +0900

    Config: 시내버스 RedisTemplate 추가 및 시리얼라이저 설정

--- src/main/java/WhatTheBus/Config/RedisConfig.java
+++ src/main/java/WhatTheBus/Config/RedisConfig.java
@@ -1,11 +1,14 @@
 package WhatTheBus.Config;
 
-import WhatTheBus.DTO.ReceiveLocData;
+import WhatTheBus.DTO.Bus.BusStopData;
+import WhatTheBus.DTO.Shuttle.ReceiveLocData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -19,6 +22,21 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, BusStopData> busRedisTemplate(RedisConnectionFactory factory, ObjectMapper objectMapper) {
+        RedisTemplate<String, BusStopData> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        Jackson2JsonRedisSerializer<BusStopData> serializer = new Jackson2JsonRedisSerializer<>(BusStopData.class);
+        serializer.setObjectMapper(objectMapper);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        template.afterPropertiesSet();
         return template;
     }
 }
